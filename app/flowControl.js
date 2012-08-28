9fd048e22ec7fe3a19cbca0e9a1682f31e2c8196
Andy Beeching <andybeeching@gmail.com>
2012-08-28 13:18:04 +0100

    Realized flow control tests

--- app/flowControl.js
+++ app/flowControl.js
@@ -3,6 +3,10 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     fizzBuzz : function(num) {
+      // early exclusion
+      if (!num) { return false; }
+
+      // Move along, nothing to see...
       // write a function that receives a number as its argument;
       // if the number is divisible by 3, the function should return 'fizz';
       // if the number is divisible by 5, the function should return 'buzz';
@@ -11,6 +15,13 @@ define(function() {
       //
       // otherwise the function should return the number, or false if no number
       // was provided
+      var result = "";
+
+      if (!(num % 3)) { result += "fizz"; }
+
+      if (!(num % 5)) { result += "buzz"; }
+
+      return result || num;
     }
   };
 });
