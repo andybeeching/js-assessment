a463bb4664a0cddf0a4832b27ba20f6605e17b60
Andy Beeching <andybeeching@gmail.com>
2012-08-28 11:06:11 +0100

    Realizing object implementation

--- app/objects.js
+++ app/objects.js
@@ -3,15 +3,24 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     alterContext : function(fn, obj) {
-
+      // apply the desired context
+      return fn.apply(obj, arguments);
     },
 
     alterObjects : function(constructor, greeting) {
-
+      // mutate shared prototype
+      constructor.prototype.greeting = greeting;
     },
 
     iterate : function(obj) {
-
+      // shield inherited properties from iteration
+      var result = [], key;
+      for (key in obj) {
+        if (obj.hasOwnProperty(key)) {
+          result.push(key + ": " + obj[key]);
+        }
+      }
+      return result;
     }
   };
 });
