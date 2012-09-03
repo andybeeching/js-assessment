196c308d0c7cadfbf44b1c95a8dfd07d6107b0ff
Andy Beeching <andybeeching@gmail.com>
2012-09-03 11:10:04 +0100

    Realizing async interface

--- app/async.js
+++ app/async.js
@@ -3,11 +3,25 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define([ 'jquery' ], function($) {
   return {
     async : function(value) {
-
+      return {
+        then: function (callback) {
+          callback && callback(value);
+        }
+      }
     },
 
     manipulateRemoteData : function(url) {
+      return {
+        then: function (callback) {
+          $.get(url, function (data) {
+            var result = data.people.map(function (item) {
+              return item["name"];
+            });
 
+            callback && callback(result.sort());
+          });
+        }
+      };
     }
   };
 });
